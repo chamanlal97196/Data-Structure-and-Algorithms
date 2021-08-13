7cfc1e9e5c62efa6822dee790b0fa9ea02947d44
chamanlal97196 <chamanlal97196@gmail.com>
2021-08-13 12:23:34 +0530

    Create 1.ArrayImplementationOfStack.cpp

---
+++ StackQuestions/1.ArrayImplementationOfStack.cpp
@@ -0,0 +1,55 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct myStack{
+	int *arr;
+	int cap;
+	int top;
+	myStack(int c){
+		cap=c;
+		arr=new int[cap];
+		top=-1;
+		
+	}
+	void push(int x){
+		if(top>cap-1) cout<<"OverFlow "<<endl;
+		top++;
+		arr[top]=x;
+	}
+	int pop(){
+		if(top==-1) cout<<"UnderFlow "<<endl;
+		int res=arr[top];
+		top--;
+		return res;
+	}
+	int peek(){
+		return arr[top];
+	}
+	int size(){
+		return (top+1);
+	}
+	bool isEmpty(){
+		return (top==-1);
+	}
+	
+};
+
+
+int main()
+{
+	// std::cout << "Hello, World" << std::endl;
+	myStack s(5);
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	cout<<s.peek()<<endl;
+	cout<<s.size()<<endl;
+	cout<<s.pop()<<endl;
+	cout<<s.pop()<<endl;
+	cout<<s.size()<<endl;
+	cout<<s.peek()<<endl;
+	cout<<s.isEmpty()<<endl;
+	return 0;
+}
