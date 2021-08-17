ec88da35407ac6437605f8c537ecb2e3f51f41e1
chamanlal97196 <chamanlal97196@gmail.com>
2021-08-17 12:13:29 +0530

    Update 1.firstOccurence.cpp

--- BinarySearch/1.firstOccurence.cpp
+++ BinarySearch/1.firstOccurence.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOcc(vector<int>&arr, int x){
+	int high=arr.size()-1;
+	int low=0;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>x){
+			high=mid-1;
+		}
+		else if(arr[mid]<x){
+			low=mid;
+		}
+		else{
+			if(mid==0 || arr[mid-1]!=arr[mid]) return mid;
+			else high = mid-1;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	// std::cout << "Hello, World" << std::endl;
+	vector<int>v={5,33,33,38,44,56,87};
+	cout<<firstOcc(v,33)<<endl;
+	
+	
+	return 0;
+}
 
