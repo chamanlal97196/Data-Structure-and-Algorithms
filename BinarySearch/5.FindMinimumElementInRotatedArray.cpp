c037cda2ab8d86a7e5a63abcee961ebb8070a147
chamanlal97196 <chamanlal97196@gmail.com>
2021-09-25 11:32:40 +0530

    Create 5.FindMinimumElementInRotatedArray.cpp

---
+++ BinarySearch/5.FindMinimumElementInRotatedArray.cpp
@@ -0,0 +1,80 @@
+/*
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+All the integers of nums are unique.
+nums is sorted and rotated between 1 and n times.
+
+*/
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        /*int start=0,end=num.size()-1;
+        
+        while (start<end) {
+            if (num[start]<num[end])
+                return num[start];
+            
+            int mid = (start+end)/2;
+            
+            if (num[mid]>=num[start]) {
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return num[start];*/
+        int n=nums.size();
+        int start=0;
+        int end=nums.size()-1;
+        int mid;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            int next=(mid+1)%n;
+            int prev=(mid+n-1)%n;
+            if(nums[start]<nums[end]){
+                return nums[start];
+            }
+            if(nums[prev]>=nums[mid] and nums[mid]<=nums[next]){
+                return nums[mid];
+            }
+            else if(nums[start]<=nums[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            } 
+        }
+        return -1;
+    }
+};
